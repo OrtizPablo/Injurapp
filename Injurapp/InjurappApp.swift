52c46ffb983e2eba6465ffacd6918056169d37d1
Pablo Ortiz <pabloortizrodriguez1995@gmail.com>
2024-05-07 17:57:30 +0100

    IHT-11: create iOS project

---
+++ Injurapp/InjurappApp.swift
@@ -0,0 +1,17 @@
+//
+//  InjurappApp.swift
+//  Injurapp
+//
+//  Created by Pablo Ortiz Rodríguez on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct InjurappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
